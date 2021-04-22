ffb86003928066b6c3940510e727b05662cc42be
Jon Reades <jon@reades.com>
2021-04-22 08:53:19 +0100

    Updating with new libs and also anticipating some of the changes for JupyterLab 3 and 4

--- docker/docker.sh
+++ docker/docker.sh
@@ -12,15 +12,15 @@
 # particular location
 WORK_DIR="$HOME/Documents/git"
 DOCKER_NM="sds"
-DOCKER_IMG="jreades/sds:1.0.0"
+DOCKER_IMG="jreades/sds:2020b"
 JUPYTER_PWD="sha1:288f84f833b0:7645388b889d84efbb2716d646e5eadd78b67d10"
 
 if [ $1 = "start" ]; then
 	echo "Starting up..."
 	if [[ "$OSTYPE" == "msys" ]]; then
-		winpty docker run --rm -d --name $DOCKER_NM -p 8888:8888 -v "$WORK_DIR":/home/jovyan/work $DOCKER_IMG start.sh jupyter lab --LabApp.password=$JUPYTER_PWD
+		winpty docker run --rm -d --name $DOCKER_NM -p 8888:8888 -v "$WORK_DIR":/home/jovyan/work $DOCKER_IMG start.sh jupyter lab --ServerApp.password=$JUPYTER_PWD --LabApp.password=$JUPYTER_PWD
 	else
-		docker run --rm -d --name $DOCKER_NM -p 8888:8888 -v "$WORK_DIR":/home/jovyan/work $DOCKER_IMG start.sh jupyter lab --LabApp.password=$JUPYTER_PWD
+		docker run --rm -d --name $DOCKER_NM -p 8888:8888 -v "$WORK_DIR":/home/jovyan/work $DOCKER_IMG start.sh jupyter lab --ServerApp.password=$JUPYTER_PWD --LabApp.password=$JUPYTER_PWD
 	fi
 	echo "*Should* have started on localhost:8888"
 else
