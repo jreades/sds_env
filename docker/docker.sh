681a8ce31d964e1b13beb45d26235e7186ac5f31
Jon Reades <jon@reades.com>
2021-07-13 18:06:46 +0100

    Updated to new version.

--- docker/docker.sh
+++ docker/docker.sh
@@ -12,11 +12,11 @@
 # particular location
 WORK_DIR="$HOME/Documents/git"
 DOCKER_NM="sds"
-DOCKER_IMG="jreades/sds:2020b"
+DOCKER_IMG="jreades/sds:2021c"
 JUPYTER_PWD="sha1:288f84f833b0:7645388b889d84efbb2716d646e5eadd78b67d10"
 
 if [ $1 = "start" ]; then
-	echo "Starting up..."
+	echo "Starting ${DOCKER_IMG}..."
 	if [[ "$OSTYPE" == "msys" ]]; then
 		winpty docker run --rm -d --name $DOCKER_NM -p 8888:8888 -v "$WORK_DIR":/home/jovyan/work $DOCKER_IMG start.sh jupyter lab --ServerApp.password=$JUPYTER_PWD --LabApp.password=$JUPYTER_PWD
 	else
@@ -27,5 +27,5 @@ else
 	echo "Shutting down..."
 	CONTAINER=$(docker ps -aq -f name=$DOCKER_NM)
 	docker rm -f $CONTAINER
-	echo "*Should* have now shut down"
+	echo "*Should* have now shut down ${DOCKER_IMG}"
 fi
